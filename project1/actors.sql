5bc828882bdd3b8bde6cb9b15122fa9fa1f78490
rchen93 <rchen93@ucla.edu>
2015-01-10 16:31:04 -0800

    Added Part B

---
+++ project1/actors.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Actors (Name varchar(40), Movie varchar(80), Year int, Role varchar(40));
+
+LOAD DATA LOCAL INFILE '~/data/actors.csv' INTO TABLE Actors
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+--Select all actors who starred in the movie 'Die Another Day'
+SELECT Name
+FROM Actors
+WHERE Movie = "Die Another Day";
+
+DROP TABLE IF EXISTS Actors;
