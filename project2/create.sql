9b15ced87347433cea0f2f8ea5c282ab7aab73be
rchen93 <rchen93@ucla.edu>
2015-01-31 17:16:13 -0800

    Fixed order of creation/deletion of tables

--- project2/create.sql
+++ project2/create.sql
@@ -1,4 +1,14 @@
-CREATE TABLE Item (iid bigint,
+CREATE TABLE Seller (sid varchar(40) NOT NULL,
+				     rating int,
+				     PRIMARY KEY(sid)) ENGINE=INNODB;
+
+CREATE TABLE Bidder (bid bigint NOT NULL,
+					rating int,
+					location varchar(256),
+					country varchar(256),
+					PRIMARY KEY(bid)) ENGINE=INNODB;
+
+CREATE TABLE Item (iid bigint NOT NULL,
 				   name varchar(256),
 				   currently decimal(8,2),
 				   first_bid decimal(8,2),
@@ -7,32 +17,22 @@ CREATE TABLE Item (iid bigint,
 				   started timestamp,
 				   ends timestamp,
 				   description varchar(4000),
-				   sid bigint,
+				   sid varchar(40),
 				   location varchar(256),
 				   country varchar(256),
 				   latitude varchar(256),
 				   longitude varchar(256),
-				   FOREIGN KEY (sid) references Seller(sid),
-				   PRIMARY KEY(iid));
+				   FOREIGN KEY (sid) REFERENCES Seller(sid),
+				   PRIMARY KEY(iid)) ENGINE=INNODB;
 
-CREATE TABLE ItemCategory (iid bigint,
+CREATE TABLE ItemCategory (iid bigint NOT NULL,
 						  Category varchar(4000),
-						  FOREIGN KEY (iid) references Item(iid));
+						  FOREIGN KEY (iid) REFERENCES Item(iid)) ENGINE=INNODB;
 
-CREATE TABLE Seller (sid varchar(40),
-				     rating int,
-				     PRIMARY KEY(sid));
-
-CREATE TABLE Bidder (bid bigint,
-					rating int,
-					location varchar(256),
-					country varchar(256),
-					PRIMARY KEY(bid));
-
-CREATE TABLE Bid (iid bigint,
-				  bid int,
-				  time timestamp,
+CREATE TABLE Bid (iid bigint NOT NULL,
+				  bid int NOT NULL,
+				  time timestamp NOT NULL,
 				  amount decimal(8,2),
-				  PRIMARY KEY(iid, bid, time),
-				  FOREIGN KEY (iid) references Item(iid),
-				  FOREIGN KEY (bid) references Bidder(bid));
+				  FOREIGN KEY (iid) REFERENCES Item(iid),
+				  FOREIGN KEY (bid) REFERENCES Bidder(bid),
+				  PRIMARY KEY(iid, bid, time)) ENGINE=INNODB;
