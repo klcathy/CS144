8c94982118fccf45b61220ac975c8f636c49e23a
rchen93 <rchen93@ucla.edu>
2015-02-07 00:23:41 -0800

    Finished Lucene indexes (untested)

--- project3a/src/edu/ucla/cs/cs144/Indexer.java
+++ project3a/src/edu/ucla/cs/cs144/Indexer.java
@@ -1,5 +1,7 @@
 package edu.ucla.cs.cs144;
 
+import java.util.*;
+
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.File;
@@ -27,45 +29,119 @@ public class Indexer {
     /** Creates a new instance of Indexer */
     public Indexer() {
     }
+
+    private IndexWriter indexWriter = null;
+
+    public IndexWriter getIndexWriter()  {
+        if (indexWriter == null) {
+            try {
+                Directory indexDir = FSDirectory.open(new File("/var/lib/lucene"));
+                IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, new StandardAnalyzer());
+                indexWriter = new IndexWriter(indexDir, config);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+        return indexWriter;
+    }
+
+    public void closeIndexWriter() throws IOException {
+        if (indexWriter != null) {
+            try {
+                indexWriter.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
  
     public void rebuildIndexes() {
 
         Connection conn = null;
 
         // create a connection to the database to retrieve Items from MySQL
-	try {
-	    conn = DbManager.getConnection(true);
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+        try {
+            conn = DbManager.getConnection(true);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        Statement itemStmt = conn.createStatement();        // Item table
+        Statement categoryStmt = conn.createStatement();    // Category table
+
+        ResultSet itemRS = itemStmt.executeQuery("SELECT iid, name, description FROM Item");
+        ResultSet categoryRS = categoryStmt.executeQuery("SELECT * FROM ItemCategory");
+
+        // Stores item -> category relations
+        HashMap<String, String> itemCategoryMap = new HashMap<String, String>();
+
+        // Concatenate all categories of an item into one string
+        while (categoryRS.next()) {
+            String itemId = categoryRS.getString("iid");
+            String currentCategory = categoryRS.getString("category");
+
+            if (itemCategoryMap.containsKey(itemId)) {
+                itemCategoryMap.put(itemId, itemCategoryMap.get(itemId) + " " + currentCategory);
+            } else {
+                itemCategoryMap.put(itemId, currentCategory);
+            }
+        }
+
+        // Index items with correspodning categories
+        while (itemRS.next()) {
+            IndexWriter writer = getIndexWriter();
+            Document doc = new Document();
+
+            // Individual keyword search
+            String itemId = itemRS.getString("iid");
+            String name = itemRS.getString("name");
+            String description = itemRS.getString("description");
+            String categories = itemCategoryMap.get(itemId);
+
+            // Basic keyword search
+            String fullSearchableText = name + " " + description + " " + categories;
+
+            doc.add(new StringField("iid", itemId, Field.Store.YES));
+            doc.add(new StringField("name", name, Field.Store.YES));
+            doc.add(new TextField("content", fullSearchableText, Field.Store.NO));
+            writer.addDocument(doc);
+        }
+
+        // Do I need all these closes?
+        itemRS.close();
+        categoryRS.close();
+        itemStmt.close();
+        categoryStmt.close();
+
+        closeIndexWriter();
 
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
+        /*
+         * Add your code here to retrieve Items using the connection
+         * and add corresponding entries to your Lucene inverted indexes.
+             *
+             * You will have to use JDBC API to retrieve MySQL data from Java.
+             * Read our tutorial on JDBC if you do not know how to use JDBC.
+             *
+             * You will also have to use Lucene IndexWriter and Document
+             * classes to create an index and populate it with Items data.
+             * Read our tutorial on Lucene as well if you don't know how.
+             *
+             * As part of this development, you may want to add
+             * new methods and create additional Java classes.
+             * If you create new classes, make sure that
+             * the classes become part of "edu.ucla.cs.cs144" package
+             * and place your class source files at src/edu/ucla/cs/cs144/.
          *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add 
-         * new methods and create additional Java classes. 
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 * 
-	 */
+         */
 
 
-        // close the database connection
-	try {
-	    conn.close();
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+            // close the database connection
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }    
 
     public static void main(String args[]) {
